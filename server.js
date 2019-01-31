e64929b70ba0762bb69439cb7f7c766c9ac8627c
ducky <ivan@getin.co.il>
2019-01-31 04:31:55 +0200

    ??

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');  
 const app = express();  
-app.use(express.static(__dirname + '/dist'));  
+app.use(express.static(__dirname + '/dist/cinema-app'));  
 app.all('*', function (req, res) {  
   res.status(200).sendFile(__dirname + '/dist/cinema-app/index.html');  
 });  
